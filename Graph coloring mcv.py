de4d3ce739c6afb4efc50187aec60b4723525251
hunain631 <hunainsaeed631@gmail.com>
2021-05-06 20:28:27 +0500

    first commit

---
+++ Graph coloring mcv.py
@@ -0,0 +1,68 @@
+pk = {}
+vertex = {}
+
+color = {1:'Red', 2:'Green', 3:'Blue'}
+
+def printColor():
+    for i in vertex.keys():
+        print(i, " : ", color[vertex[i]])
+
+
+def colorGraph():
+    start = pickMRV(list(pk.keys()))
+
+    vertex[start] = 1
+    
+    for j in pk.keys():
+        ver = pickMRV(pk[j])
+        if ver != 0:
+            vertex[ver] = safeColor(ver)
+        
+    printColor()
+
+    return
+
+def safeColor(ver):
+    col = []
+    for i in pk[ver]:
+        col.append(vertex[i])
+    
+    for i in color.keys():
+        if i not in col:
+            return i
+
+
+def pickMRV(ver):
+    minNode = 0
+    lenNode = 0
+    for i in ver:
+        if vertex[i] == 0:
+            if lenNode < len(pk[i]):        
+                lenNode = len(pk[i])
+                minNode = i
+    
+    return minNode
+
+
+def main():
+    global pk
+    global vertex
+    pk = {
+        'Sindh':        ['Baluchistan', 'Punjab'],
+        'Baluchistan':  ['NWFP', 'Punjab', 'Sindh'],
+        'Punjab':       ['Sindh', 'Baluchistan', 'NWFP', 'Kashmir'],
+        'NWFP':         ['Baluchistan', 'Punjab', 'Kashmir'],
+        'Kashmir':      ['Punjab', 'NWFP']
+    }
+
+    vertex = {
+        'Sindh': 0,
+        'Baluchistan': 0,
+        'Punjab': 0,
+        'NWFP': 0,
+        'Kashmir': 0
+    }
+
+    colorGraph()
+
+main()
